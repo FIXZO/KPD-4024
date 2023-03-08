710c50ad46e95d6571f664e6c56dbae996804333
FIXZO <108600991+FIXZO@users.noreply.github.com>
2023-03-08 16:04:02 +0800

    Add files via upload

---
+++ link.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""
+$database = "";
+
+// Create connection
+$conn = mysqli_connect("localhost","root","");
+$db = mysqli_select_db("farmasi");
+echo "databases not connected";
+?>
